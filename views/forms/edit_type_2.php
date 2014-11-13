b0418e8044030155baf70725c918fd65113336be
Sergei Gulin <s.gulin@rootmedia.ru>
2014-11-13 18:03:59 +0400

    fix 2

--- views/forms/edit_type_2.php
+++ views/forms/edit_type_2.php
@@ -8,7 +8,7 @@ Collapse::widget([
     'items' => [
         [
             'label' => $item['label'],
-            'content' => '<div class="dd-handle dd3-handle">Drag</div><div class="dd3-content">Item 17</div>', //$this->render('_edit_type_2_form', ['item' => $item]),
+            'content' => $this->render('_edit_type_2_form', ['item' => $item]),
             'contentOptions' => ['class' => 'dd-nodrag'],
         ],
     ],
